35160b9f1c38463e8b1494411bca858188b6e8dd
Anthony Rouneau <angeal1105@gmail.com>
2016-12-14 15:22:37 +0100

    Ajout d'un exemple de projet à utiliser avec PyCharm

---
+++ Ressources/exemple_projet/package2/executable.py
@@ -0,0 +1,4 @@
+from test.mon_test import my_function
+
+if __name__ == "__main__":
+    print(my_function("Formation python", 14, 12))
---
+++ Ressources/exemple_projet/test/mon_test.py
@@ -0,0 +1,13 @@
+
+
+def my_function(title: str, x: int, y: int) -> str:
+    """
+    :param title: Le titre de la chaine de caractère résultat
+    :param x: Premier élément de la chaine de caractère résultat
+    :param y: Deuxième élément de la chaine de caracère résultat
+    :return: La chaine de caractère résultat composée du titre,
+             suivi de deux retours à la ligne, suivi de x et y séparés par un espace
+
+    Construis une chaine de caractères utilisant le titre donné ainsi que x et y
+    """
+    return title + "\n" + str(x) + " " + str(y)
